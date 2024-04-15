21ab19106f04cab75acaebb87dea6f6ffb481ece
yibishufange <yibishufange@gmail.com>
2024-04-15 10:02:06 +0800

    add gin context

---
+++ ginctx/context.go
@@ -0,0 +1,401 @@
+package ginctx
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"runtime/debug"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
+	"github.com/satori/go.uuid"
+
+	"github.com/zulu-network/zulu-go-sdk/errors"
+	"github.com/zulu-network/zulu-go-sdk/log"
+	"github.com/zulu-network/zulu-go-sdk/utils"
+)
+
+const (
+	LogKeyRequestID = "requestID"
+	LogKeyAction    = "action"
+
+	ActionExecStartTime = "startTime"
+)
+
+// Context context
+type Context struct {
+	*gin.Context
+	*log.Logger
+}
+
+type User struct {
+	ID   string
+	Name string
+}
+
+type UserInfo struct {
+	User   User
+	Roles  []Role
+	Domain Domain
+}
+
+type Role struct {
+	ID   string
+	Type string
+}
+
+type Domain struct {
+	ID   string
+	Name string
+}
+
+// NewContext create a new context with gin context
+func NewHttpContext(inner *gin.Context) *Context {
+	return &Context{inner, log.With(log.Any(LogKeyRequestID, uuid.NewV4().String()))}
+}
+
+// NewContext create a new context with log
+func NewContextWithLog(log *log.Logger) *Context {
+	return &Context{&gin.Context{}, log}
+}
+
+// NewTraceContext trace contest with start time and field
+func NewTraceContext(requestID string, field ...log.Field) *Context {
+	c := &Context{&gin.Context{}, log.With(field...)}
+	c.SetRequestID(requestID)
+	c.SetTraceStartTime()
+	return c
+}
+
+// NewContextEmpty create a new empty context
+func NewContextEmpty() *Context {
+	return &Context{&gin.Context{}, log.L()}
+}
+
+// SetTraceStartTime set action exec start time
+func (c *Context) SetTraceStartTime() {
+	t := time.Now()
+	c.Set(ActionExecStartTime, t)
+	c.Logger = c.Logger.With(log.Any("startTime", t))
+}
+
+// TraceCostTime add from start trace to this func cost time
+func (c *Context) TraceCostTime(event string) {
+	if v, ok := c.Get(ActionExecStartTime); ok {
+		if t, ok := v.(time.Time); ok {
+			c.Logger = c.Logger.With(log.Any(event+" costTime", time.Since(t)))
+		}
+	}
+}
+
+// SetRequestID  set log requestID
+func (c *Context) SetRequestID(requestID string) {
+	if requestID == "" {
+		requestID = uuid.NewV4().String()
+	}
+	c.Logger = c.Logger.With(log.Any(LogKeyRequestID, requestID))
+	c.Set(LogKeyRequestID, requestID)
+}
+
+// SetAction set exec action func
+func (c *Context) SetAction(value interface{}) {
+	c.Logger = c.Logger.With(log.Any(LogKeyAction, value))
+}
+
+// GetRequestID get log requestID
+func (c *Context) GetRequestID() string {
+	if _, ok := c.Get(LogKeyRequestID); ok {
+		return c.GetString(LogKeyRequestID)
+	}
+	return ""
+}
+
+func (c *Context) SetInfo(key string, value interface{}) {
+	c.Logger = c.Logger.With(log.Any(key, value))
+}
+
+// SetNamespace sets namespace into context
+func (c *Context) SetNamespace(ns string) {
+	c.Set("namespace", ns)
+}
+
+// GetNamespace gets namespace from context if exists
+func (c *Context) GetNamespace() string {
+	return c.GetString("namespace")
+}
+
+// SetUser sets user into context
+func (c *Context) SetUser(user User) {
+	c.Set("user", user)
+}
+
+// GetUser gets user from context if exists
+func (c *Context) GetUser() User {
+	user, ok := c.Get("user")
+	if !ok {
+		return User{}
+	}
+	return user.(User)
+}
+
+// SetUser sets user info into context
+func (c *Context) SetUserInfo(info UserInfo) {
+	c.Set("userInfo", info)
+}
+
+// GetUser gets user info from context if exists
+func (c *Context) GetUserInfo() UserInfo {
+	info, ok := c.Get("userInfo")
+	if !ok {
+		return UserInfo{}
+	}
+	return info.(UserInfo)
+}
+
+// SetName sets name into context
+func (c *Context) SetName(n string) {
+	c.Set("name", n)
+}
+
+// GetName gets name from context if exists
+func (c *Context) GetName() string {
+	return c.GetString("name")
+}
+
+// GetNameFromParam gets name from param if exists
+func (c *Context) GetNameFromParam() string {
+	return c.Param("name")
+}
+
+// LoadBody loads json data from body into object and set defaults
+func (c *Context) LoadBody(obj interface{}) error {
+	err := c.BindJSON(obj)
+	if err != nil {
+		if es, ok := err.(validator.ValidationErrors); ok {
+			for _, v := range es {
+				return Error(Code(v.Tag()), Field(v.Tag(), v.Field()), Field("error", err.Error()))
+			}
+		}
+		return err
+	}
+	return utils.SetDefaults(obj)
+}
+
+func (c *Context) LoadBodyMulti(obj interface{}) error {
+	err := c.ShouldBindBodyWith(obj, binding.JSON)
+	if err != nil {
+		if es, ok := err.(validator.ValidationErrors); ok {
+			for _, v := range es {
+				return Error(Code(v.Tag()), Field(v.Tag(), v.Field()), Field("error", err.Error()))
+			}
+		}
+		return err
+	}
+	return utils.SetDefaults(obj)
+}
+
+type sucResponse struct {
+	Success bool `json:"success"`
+}
+
+// PackageResponse PackageResponse
+func PackageResponse(res interface{}) (int, interface{}) {
+	if res == nil {
+		res = &sucResponse{
+			Success: true,
+		}
+	}
+	return http.StatusOK, res
+}
+
+// PopulateFailedResponse PopulateFailedResponse
+func PopulateFailedResponse(cc *Context, err error, abort bool) {
+	var code string
+	var status int
+	switch e := err.(type) {
+	case errors.Coder:
+		code = e.Code()
+		status = getHTTPStatus(Code(e.Code()))
+	default:
+		code = ErrUnknown
+		status = http.StatusInternalServerError
+	}
+
+	cc.Logger.Error("process failed.", log.Code(err))
+
+	body := gin.H{
+		"code":          code,
+		"message":       err.Error(),
+		LogKeyRequestID: cc.GetRequestID(),
+	}
+	if abort {
+		cc.AbortWithStatusJSON(status, body)
+	} else {
+		cc.JSON(status, body)
+	}
+}
+
+// HandlerFunc HandlerFunc
+type HandlerFunc func(c *Context) (interface{}, error)
+type LockFunc func(ctx context.Context, name string, ttl int64) (string, error)
+type UnlockFunc func(ctx context.Context, name, version string)
+
+// Wrapper Wrapper
+// TODO: to use gin.HandlerFunc ?
+func Wrapper(handler HandlerFunc) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		cc := NewHttpContext(c)
+		cc.Set("startTime", time.Now())
+		defer func() {
+			if r := recover(); r != nil {
+				err, ok := r.(error)
+				if !ok {
+					err = Error(ErrUnknown, Field("error", r))
+				}
+				cc.Logger.Info("handle a panic", log.Code(err), log.Error(err), log.Any("panic", string(debug.Stack())))
+				PopulateFailedResponse(cc, err, false)
+			}
+		}()
+		res, err := handler(cc)
+		if err != nil {
+			cc.Logger.Error("failed to handler request", log.Code(err), log.Error(err))
+			PopulateFailedResponse(cc, err, false)
+			return
+		}
+		cc.Logger.Debug("process success", log.Any("response", _toJsonString(res)))
+		// unlike JSON, does not replace special html characters with their unicode entities. eg: JSON(&)->'\u0026' PureJSON(&)->'&'
+		cc.PureJSON(PackageResponse(res))
+	}
+}
+
+// WrapperWithLock wrap handler with lock
+func WrapperWithLock(lockFunc LockFunc, unlockFunc UnlockFunc) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		cc := NewHttpContext(c)
+		defer func() {
+			if r := recover(); r != nil {
+				err, ok := r.(error)
+				if !ok {
+					err = Error(ErrUnknown, Field("error", r))
+				}
+				cc.Logger.Info("handle a panic", log.Code(err), log.Error(err), log.Any("panic", string(debug.Stack())))
+				PopulateFailedResponse(cc, err, false)
+			}
+		}()
+		ctx := context.Background()
+		lockName := "namespace_" + cc.GetNamespace()
+		version, err := lockFunc(ctx, lockName, 0)
+		if err != nil {
+			cc.Logger.Error("failed to handler request", log.Code(err), log.Error(err))
+			PopulateFailedResponse(cc, err, true)
+			return
+		}
+		defer unlockFunc(ctx, lockName, version)
+		cc.Next()
+	}
+}
+
+func WrapperRaw(handler HandlerFunc, abort bool) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		cc := NewHttpContext(c)
+		defer func() {
+			if r := recover(); r != nil {
+				err, ok := r.(error)
+				if !ok {
+					err = Error(ErrUnknown, Field("error", r))
+				}
+				cc.Logger.Info("handle a panic", log.Code(err), log.Error(err))
+				PopulateFailedResponse(cc, err, abort)
+			}
+		}()
+		res, err := handler(cc)
+		if err != nil {
+			cc.Logger.Error("failed to handler request", log.Code(err), log.Error(err))
+			PopulateFailedResponse(cc, err, abort)
+			return
+		}
+		if res == nil {
+			return
+		}
+		if data, ok := res.([]byte); ok {
+			cc.Data(http.StatusOK, "application/octet-stream", data)
+		} else {
+			cc.Logger.Error("failed to convert data to []byte")
+			PopulateFailedResponse(cc, Error(ErrUnknown), abort)
+		}
+	}
+}
+
+func WrapperNative(handler HandlerFunc, abort bool) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		cc := NewHttpContext(c)
+		defer func() {
+			if r := recover(); r != nil {
+				err, ok := r.(error)
+				if !ok {
+					err = Error(ErrUnknown, Field("error", r))
+				}
+				cc.Logger.Info("handle a panic", log.Code(err), log.Error(err))
+				PopulateFailedResponse(cc, err, abort)
+			}
+		}()
+		_, err := handler(cc)
+		if err != nil {
+			cc.Logger.Error("failed to handler request", log.Code(err), log.Error(err))
+			PopulateFailedResponse(cc, err, abort)
+			return
+		}
+	}
+}
+
+func _toJsonString(obj interface{}) string {
+	data, _ := json.Marshal(obj)
+	return string(data)
+}
+
+func WrapperMis(handler HandlerFunc) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		cc := NewHttpContext(c)
+		defer func() {
+			if r := recover(); r != nil {
+				err, ok := r.(error)
+				if !ok {
+					err = Error(ErrUnknown, Field("error", r))
+				}
+				cc.Logger.Info("handle a panic", log.Code(err), log.Error(err), log.Any("panic", string(debug.Stack())))
+				PopulateFailedMisResponse(cc, err, false)
+			}
+		}()
+		res, err := handler(cc)
+		if err != nil {
+			cc.Logger.Error("failed to handler request", log.Code(err), log.Error(err))
+			PopulateFailedMisResponse(cc, err, false)
+			return
+		}
+		cc.Logger.Debug("process success", log.Any("response", _toJsonString(res)))
+		// unlike JSON, does not replace special html characters with their unicode entities. eg: JSON(&)->'\u0026' PureJSON(&)->'&'
+		cc.PureJSON(http.StatusOK, gin.H{
+			"status": 0,
+			"msg":    "ok",
+			"data":   res,
+		})
+	}
+}
+
+// PopulateFailedMisResponse PopulateFailedMisResponse
+func PopulateFailedMisResponse(cc *Context, err error, abort bool) {
+	var status int = http.StatusOK
+	cc.Logger.Error("process failed.", log.Code(err))
+
+	body := gin.H{
+		"status": 1,
+		"msg":    err.Error(),
+	}
+	if abort {
+		cc.AbortWithStatusJSON(status, body)
+	} else {
+		cc.JSON(status, body)
+	}
+}
--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/creasty/defaults v1.4.0
 	github.com/panjf2000/ants/v2 v2.9.1
 	github.com/qiangxue/fasthttp-routing v0.0.0-20160225050629-6ccdc2a18d87
-	github.com/stretchr/testify v1.8.2
+	github.com/stretchr/testify v1.8.4
 	github.com/valyala/fasthttp v1.49.0
 	go.uber.org/zap v1.16.0
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
@@ -17,24 +17,46 @@ require (
 )
 
 require (
+	github.com/baetyl/baetyl-go/v2 v2.2.3
 	github.com/docker/go-connections v0.4.0
+	github.com/gin-gonic/gin v1.9.1
+	github.com/go-playground/validator/v10 v10.19.0
 	github.com/pkg/errors v0.9.1
+	github.com/satori/go.uuid v1.2.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/bytedance/sonic v1.9.1 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-ozzo/ozzo-routing v2.1.4+incompatible // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/gddo v0.0.0-20210115222349-20d68f94ee1f // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.16.3 // indirect
-	github.com/kr/pretty v0.2.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	go.uber.org/atomic v1.6.0 // indirect
 	go.uber.org/multierr v1.5.0 // indirect
+	golang.org/x/arch v0.3.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 )
