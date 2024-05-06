93139368c6ab72b88424a0d1566698af99f236ac
yibishufange <yibishufange@gmail.com>
2024-05-06 22:44:05 +0800

    add token info  db

---
+++ database/token.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"github.com/zulu-network/zulu-go-sdk/spec"
+)
+
+func (db *Database) CreateZuluTokenInfo(ti *spec.ZuluTokenInfo) error {
+	return db.DB.Create(ti).Error
+}
+
+func (db *Database) GetZuluTokenInfo(address string) (*spec.ZuluTokenInfo, error) {
+	var tokenInfo spec.ZuluTokenInfo
+	if err := db.DB.Where("address = ?", address).Find(&tokenInfo).Error; err != nil {
+		return nil, err
+	}
+	return &tokenInfo, nil
+}
