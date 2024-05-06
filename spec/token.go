a557c8a88551f744772097ab5511f2215be37fe6
yibishufange <yibishufange@gmail.com>
2024-05-06 16:49:50 +0800

    add token info spec

---
+++ spec/token.go
@@ -0,0 +1,16 @@
+package spec
+
+import (
+	"gorm.io/gorm"
+)
+
+type ZuluTokenInfo struct {
+	gorm.Model
+	Address   string `gorm:"unique;not null;"`
+	Hash      string `gorm:"not null;"`
+	Name      string `gorm:"not null;"`
+	Symbol    string `gorm:"not null;"`
+	Decimals  uint8  `gorm:"not null;"`
+	Cap       string `gorm:"not null;"`
+	TokenType uint8  `gorm:"not null;"`
+}
