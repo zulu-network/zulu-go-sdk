6cf94d0e739c1f4a4d52a6f19abf906cefcf20c6
yibishufange <yibishufange@gmail.com>
2024-07-02 18:13:37 +0800

    modify zulu token info

--- spec/token.go
+++ spec/token.go
@@ -22,20 +22,20 @@ const (
 
 type ZuluTokenInfo struct {
 	gorm.Model
-	Name          string `gorm:"not null;index:idx_nsdmt,unique"`
-	Symbol        string `gorm:"not null;index:idx_nsdmt,unique"`
-	RunesID       string `gorm:"not null;"`
-	Decimals      uint8  `gorm:"not null;index:idx_nsdmt,unique"`
-	MaxSupply     string `gorm:"not null;index:idx_nsdmt,unique"`
-	TokenType     int    `gorm:"not null;index:idx_nsdmt,unique"`
-	CoboName      string `gorm:"unique;not null;"`
-	ChainCode     string `gorm:"not null"`
-	ChainID       string `gorm:"not null;index:idx_tokenaddress_chainid,unique"`
-	TokenAddress  string `gorm:"not null;index:idx_tokenaddress_chainid,unique"`
-	TokenHash     string `gorm:"not null;"`
-	BridgeFee     string `gorm:"not null;"`
-	MinimumAmount string `gorm:"not null;"`
-	LogoUrl       string `gorm:"not null;"`
+	FromTokenName      string `gorm:"unique;not null;"`
+	FromChainCode      string `gorm:"not null"`
+	FromChainID        string `gorm:"not null;index:idx_tokenaddress_chainid,unique"`
+	FromTokenAddress   string `gorm:"not null"`
+	DestTokenName      string `gorm:"not null;"`
+	DestTokenSymbol    string `gorm:"not null;"`
+	DestTokenDecimals  uint8  `gorm:"not null;"`
+	DestTokenMaxSupply string `gorm:"not null;"`
+	DestTokenType      int    `gorm:"not null;"`
+	DestTokenAddress   string `gorm:"not null;index:idx_tokenaddress_chainid,unique"`
+	DestTokenHash      string `gorm:"not null;"`
+	BridgeFee          string `gorm:"not null;"`
+	MinimumAmount      string `gorm:"not null;"`
+	LogoUrl            string `gorm:"not null;"`
 }
 
 // Deprecated
