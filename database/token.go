cee082134fbeed60d6ab1cac2f48881e86d8a198
yibishufange <yibishufange@gmail.com>
2024-07-02 13:27:48 +0800

    modify token info api

--- database/token.go
+++ database/token.go
@@ -8,20 +8,35 @@ func (db *Database) CreateZuluTokenInfo(ti *spec.ZuluTokenInfo) error {
 	return db.DB.Create(ti).Error
 }
 
-func (db *Database) GetZuluTokenInfo(address string) (*spec.ZuluTokenInfo, error) {
+func (db *Database) GetZuluTokenInfo(name, symbol string, decimals uint8, maxSupply string, tokenType int) (*spec.ZuluTokenInfo, error) {
 	var tokenInfo spec.ZuluTokenInfo
-	if err := db.DB.Where("address = ?", address).First(&tokenInfo).Error; err != nil {
+	if err := db.DB.Where("name = ? AND symbol = ? AND decimals = ? AND max_supply = ? AND token_type = ?", name, symbol, decimals, maxSupply, tokenType).First(&tokenInfo).Error; err != nil {
 		return nil, err
 	}
+
 	return &tokenInfo, nil
 }
 
-func (db *Database) GetZuluTokenInfoByName(name string) (*spec.ZuluTokenInfo, error) {
+func (db *Database) GetZuluTokenInfoByAddressAndChainID(address, chainID string) (*spec.ZuluTokenInfo, error) {
 	var tokenInfo spec.ZuluTokenInfo
-	if err := db.DB.Where("name = ?", name).First(&tokenInfo).Error; err != nil {
+	if err := db.DB.Where("token_address = ? AND chain_id = ?", address, chainID).First(&tokenInfo).Error; err != nil {
 		return nil, err
 	}
+
 	return &tokenInfo, nil
+}
+
+func (db *Database) GetZuluTokenInfoByCoboName(coboName string) (*spec.ZuluTokenInfo, error) {
+	var tokenInfo spec.ZuluTokenInfo
+	if err := db.DB.Where("cobo_name = ?", coboName).First(&tokenInfo).Error; err != nil {
+		return nil, err
+	}
+
+	return &tokenInfo, nil
+}
+
+func (db *Database) UpdateZuluTokenInfo(tx *spec.ZuluTokenInfo) error {
+	return db.DB.Save(tx).Error
 }
 
 func (db *Database) ListZuluTokenInfoByType(tokenType int) (*[]spec.ZuluTokenInfo, error) {
@@ -40,10 +55,12 @@ func (db *Database) ListZuluTokenInfo() (*[]spec.ZuluTokenInfo, error) {
 	return &tokenInfos, nil
 }
 
+// Deprecated
 func (db *Database) CreateL1TokenInfo(l1ti *spec.L1TokenInfo) error {
 	return db.DB.Create(l1ti).Error
 }
 
+// Deprecated
 func (db *Database) GetL1TokenInfo(name string) (*spec.L1TokenInfo, error) {
 	var l1TokenInfo spec.L1TokenInfo
 	if err := db.DB.Where("name = ?", name).First(&l1TokenInfo).Error; err != nil {
@@ -52,6 +69,7 @@ func (db *Database) GetL1TokenInfo(name string) (*spec.L1TokenInfo, error) {
 	return &l1TokenInfo, nil
 }
 
+// Deprecated
 func (db *Database) ListL1TokenInfo() (*[]spec.L1TokenInfo, error) {
 	var tokenInfos []spec.L1TokenInfo
 	if err := db.DB.Find(&tokenInfos).Error; err != nil {
--- spec/token.go
+++ spec/token.go
@@ -12,25 +12,40 @@ const (
 	TokenTypeETH  = 1000
 	TokenTypeUSDT = 1001
 	TokenTypeUSDC = 1002
+
+	TokenTypeBNB     = 2000
+	TokenTypeFDUSD   = 2001
+	TokenTypeBTCB    = 2002
+	TokenTypeBBTC    = 2003
+	TokenTypeSolvBTC = 2004
 )
 
 type ZuluTokenInfo struct {
 	gorm.Model
-	Address   string `gorm:"unique;not null;"`
-	Hash      string `gorm:"not null;"`
-	Name      string `gorm:"not null;"`
-	Symbol    string `gorm:"not null;"`
-	Decimals  uint8  `gorm:"not null;"`
-	Cap       string `gorm:"not null;"`
-	TokenType int    `gorm:"not null;"`
+	Name          string `gorm:"not null;index:idx_nsdmt,unique"`
+	Symbol        string `gorm:"not null;index:idx_nsdmt,unique"`
+	RunesID       string `gorm:"not null;"`
+	Decimals      uint8  `gorm:"not null;index:idx_nsdmt,unique"`
+	MaxSupply     string `gorm:"not null;index:idx_nsdmt,unique"`
+	TokenType     int    `gorm:"not null;index:idx_nsdmt,unique"`
+	CoboName      string `gorm:"unique;not null;"`
+	ChainCode     string `gorm:"not null"`
+	ChainID       string `gorm:"not null;index:idx_tokenaddress_chainid,unique"`
+	TokenAddress  string `gorm:"not null;index:idx_tokenaddress_chainid,unique"`
+	TokenHash     string `gorm:"not null;"`
+	BridgeFee     string `gorm:"not null;"`
+	MinimumAmount string `gorm:"not null;"`
+	LogoUrl       string `gorm:"not null;"`
 }
 
+// Deprecated
 type L1TokenInfo struct {
 	gorm.Model
 	Name      string `gorm:"unique;not null;"`
 	Symbol    string `gorm:"not null;"`
 	RunesID   string `gorm:"not null;"`
 	Type      int    `gorm:"not null;"`
+	BridgeFee string `gorm:"not null;"`
 	Decimal   int    `gorm:"not null;"`
 	MaxSupply string `gorm:"not null;"`
 }
