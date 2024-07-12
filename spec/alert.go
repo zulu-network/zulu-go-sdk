558401eff4861ef2976ccbf29cba7992b3ad58df
yibishufange <yibishufange@gmail.com>
2024-07-12 17:48:23 +0800

    add alert info struct

---
+++ spec/alert.go
@@ -0,0 +1,33 @@
+package spec
+
+import (
+	"time"
+)
+
+type AlertLevel string
+
+const (
+	Info     AlertLevel = "INFO"
+	Warning  AlertLevel = "WARNING"
+	Critical AlertLevel = "CRITICAL"
+)
+
+type AlertComponent string
+
+const (
+	APIServer AlertComponent = "API_Server"
+
+	CoboIndexder AlertComponent = "Cobo_Indexder"
+	ZuluIndexer  AlertComponent = "Zulu_Indexer"
+
+	BridgeService AlertComponent = "Bridge_Service"
+)
+
+type Alert struct {
+	Level     AlertLevel             `json:"level"`
+	Message   string                 `json:"message"`
+	Timestamp time.Time              `json:"timestamp"`
+	Component string                 `json:"component"`
+	HostName  string                 `json:"host_name"`
+	Details   map[string]interface{} `json:"details,omitempty"`
+}
